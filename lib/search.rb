34b5f0d3aae3cd88580286240511a70a2b9044c0
Jordan Roberts <jrob12921@gmail.com>
2016-04-25 15:24:03 -0400

    more

--- lib/search.rb
+++ lib/search.rb
@@ -105,10 +105,14 @@ class Search
 
   def self.get_api_key
     api_keys = [
-      ENV['JAMBASE_API_KEY'],
-      ENV['JAMBASE_API_KEY2'],
-      ENV['JAMBASE_API_KEY3'],
-      ENV['JAMBASE_API_KEY4']
+      # ENV['JAMBASE_API_KEY'],
+      # ENV['JAMBASE_API_KEY2'],
+      # ENV['JAMBASE_API_KEY3'],
+      # ENV['JAMBASE_API_KEY4'],
+      ENV['JAMBASE_API_KEY5'],
+      ENV['JAMBASE_API_KEY6'],
+      ENV['JAMBASE_API_KEY7'],
+      ENV['JAMBASE_API_KEY8']
     ]
 
     api_keys.sample
